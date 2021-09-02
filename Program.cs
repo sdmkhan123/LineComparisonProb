450005440b6364d0a0ccceb8f44279f17bc11016
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-02 21:36:07 +0530

    find the length of line between two point

--- Program.cs
+++ Program.cs
@@ -4,9 +4,19 @@ namespace LineComparisonProb
 {
     class Program
     {
+        static double lengthOfLine()
+        {
+            int x11 = Convert.ToInt32(Console.ReadLine());
+            int x12 = Convert.ToInt32(Console.ReadLine());
+            int y11 = Convert.ToInt32(Console.ReadLine());
+            int y12 = Convert.ToInt32(Console.ReadLine());
+            return Math.Sqrt(Math.Pow(x12 - x11, 2) + Math.Pow(y12 - y11, 2) * 1.0);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            double lineLength = lengthOfLine();
+            Console.WriteLine("Length of line between two point is " + lineLength);
         }
     }
 }
