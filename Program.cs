e9895b3528482c25135de3beb4d73fad3cdaacee
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-02 21:14:41 +0530

    Welcome to Line Comparison Problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisonProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+        }
+    }
+}
