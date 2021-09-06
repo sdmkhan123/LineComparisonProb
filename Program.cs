3c7f0544711c005f0d2e69e322dfa01f868d3618
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-06 19:41:24 +0530

    Compare Line length with the help of method

--- Program.cs
+++ Program.cs
@@ -12,11 +12,8 @@ namespace LineComparisonProb
             int y12 = Convert.ToInt32(Console.ReadLine());
             return Math.Sqrt(Math.Pow(x12 - x11, 2) + Math.Pow(y12 - y11, 2));
         }
-        static void Main(string[] args)
+        public static void lengthCompareMethod(double lengthOfLineFirst, double lengthOfLineSecond)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
-            double lengthOfLineFirst = lengthOfLine();
-            double lengthOfLineSecond = lengthOfLine();
             if (lengthOfLineFirst == lengthOfLineSecond)
             {
                 Console.WriteLine("Both lines have equal length");
@@ -29,6 +26,14 @@ namespace LineComparisonProb
             {
                 Console.WriteLine("Length of First line is less than length of second line");
             }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            Console.WriteLine("Enter end points of two line");
+            double lengthOfLineFirst = lengthOfLine();
+            double lengthOfLineSecond = lengthOfLine();
+            lengthCompareMethod(lengthOfLineFirst, lengthOfLineSecond);
         }
     }
 }
